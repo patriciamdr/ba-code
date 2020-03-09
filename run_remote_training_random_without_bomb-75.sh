25d6280d12f344e66d8cdb4d34db6267ac5ae756
James Lindsly <james.lindsly@gmail.com>
2020-03-09 17:45:42 +0100

    ISSUE#55: adjust run_training script for jamie

--- run_remote_training_random_without_bomb-75.sh
+++ run_remote_training_random_without_bomb-75.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 source venv/bin/activate
+export NEPTUNE_API_TOKEN=eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiMmNjNjM4MDYtZWU4MS00Nzg2LWIzZWEtMWQzNDM5OGZlNWYyIn0=
+# export TMPDIR=~/tmp
 # pip install .
 # Params are trial_number, num_episodes, opponent, resume_training, with_comm, reward_shaping
-for index in {51..75}; do
-  sbatch --partition=All --cpus-per-task=4 ./exec_training.sh "$index" 10000 'RandomAgentWithoutBomb' 0 1 1
-  sbatch --partition=All --cpus-per-task=4 ./exec_training.sh "$index" 10000 'RandomAgentWithoutBomb' 0 0 1
+for index in {51..58}; do
+  ./exec_training.sh "$index" 10000 'RandomAgentWithoutBomb' 0 1 1
+  ./exec_training.sh "$index" 10000 'RandomAgentWithoutBomb' 0 0 1
 done
